bf3c65ffa7456b1d7e221ca2c11b8517550613e5
ianbla <tag.ianblanchet@gmail.com>
2024-12-24 12:31:05 -0500

    toutes les routes a partir du constructeur

--- app/routeFactory.py
+++ app/routeFactory.py
@@ -11,21 +11,24 @@ from app.database import session_scope
 router = APIRouter()
 
 class RouteFactory:
-    def __init__(self, routename, schema, baseSchema, model):
+    """ permet de créer un objet pour générer des routes"""
+    def __init__(self, routename : str, schema : schemas, baseSchema : schemas, model : models) -> object:
         self.routename = routename
         self.schema = schema
         self.baseSchema = baseSchema
         self.model = model
         self.router = router
 
-    def create_route_get_all(self): 
+    def create_route_get_all(self) -> APIRouter: 
+        "retourne un router pour obtenir toutes les données"
         @self.router.get(f"/{self.routename}/", response_model=List[self.schema], tags=[self.routename]) 
         def read_all(db: Session = Depends(session_scope)):
             items = db.query(self.model).all() 
             return items
         return self.router
 
-    def create_route_post_new(self, fieldUniqueValidation : List[str]):
+    def create_route_post_new(self, fieldUniqueValidation : List[str]) -> APIRouter:
+        "retourne une route pour creer une nouvelle entrée"
         @self.router.post(f"/{self.routename}/" ) 
         def create_new(item : self.baseSchema, db: Session = Depends(session_scope)):
             item_dict = item.dict()
--- app/routes/route.py
+++ app/routes/route.py
@@ -8,10 +8,16 @@ router = APIRouter()
 from app.routeFactory import RouteFactory
 
 abonneRoute = RouteFactory('abonne', schemas.Abonne, schemas.BaseAbonne, models.Abonne)
-
 allAbonneRouter = abonneRoute.create_route_get_all()
 newAbonneRouter = abonneRoute.create_route_post_new(['heure', 'telephone'])
 
 centreRoute = RouteFactory('centre', schemas.Centre, schemas.Centre, models.Centre)
-
 allCentreRouter = centreRoute.create_route_get_all()
+
+usagerRoute = RouteFactory('usager', schemas.Usager, schemas.BaseUsager, models.Usager)
+allUsagerRouter = usagerRoute.create_route_get_all()
+newUsagerRouter = usagerRoute.create_route_post_new(['email'])
+
+appelRoute = RouteFactory('appel', schemas.Appel, schemas.BaseAppel, models.Appel)
+allAppelRouter = appelRoute.create_route_get_all()
+newAppelRouter = appelRoute.create_route_post_new([])
